cfbf866ac532f32e12434f45fc4da26a9160afc8
Rajiv Kumar Gupta <100814533+rajivgupta1@users.noreply.github.com>
2023-06-25 10:19:25 +1000

    Api for library

---
+++ server.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config()
+
+import express from "express";
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+
+app.use("/", (req, res) => {
+    res.json({
+        status: "success",
+        message: `Server running well`,
+    });
+});
+
+app.listen(PORT, error => {
+    error 
+    ? console.log(error.message)
+    : console.log(`server running at http://localhost:${PORT}`);
+})
